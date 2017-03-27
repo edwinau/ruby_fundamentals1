1c095c0f172ee05a0bd4714e81d83c38cf995820
EDWIN AU <edwinau@rogers.com>
2017-03-27 14:24:58 -0400

    Exercise 2 part 1 complete

---
+++ exercise2.rb
@@ -0,0 +1,6 @@
+#1. a good tip
+
+var = 55 * 0.2
+puts "A good tip is 20 percent, making the tip for a $55 meal to be $#{var}."
+puts ""
+puts "In other words, the total cost of this $55 meal including a good tip would be $#{55 + var}"
