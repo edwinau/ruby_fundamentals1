d96ff5dacf2492093d087091d3d95eebb2507688
EDWIN AU <edwinau@rogers.com>
2017-03-27 14:12:50 -0400

    Exercise 1 Booleans complete

--- exercise1.rb
+++ exercise1.rb
@@ -22,3 +22,35 @@ puts 'Edwin\'s shop'
 puts "your name is \"HELLO?\""
 
 puts "This year is #{1987+9}"
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+puts "cats" + "dogs"
+puts "cats" == "dogs"
+puts "cats" > "dogs"
+puts "cats" < "dogs"
+
+puts "abc"
+
+puts (true && true)
+puts (true && false)
+puts (false && true)
+puts (false && false)
+
+puts 123
+
+puts (true || true)
+puts (true || false)
+puts (false || true)
+puts (false || false)
+
+puts "xyz"
+
+puts !true
+puts !!10
+puts !10
+puts !false
+puts !nil
