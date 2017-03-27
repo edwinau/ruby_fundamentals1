e9bf9162908a5096d7b43d058becbe18380d13e6
EDWIN AU <edwinau@rogers.com>
2017-03-27 14:34:45 -0400

    Exercise 2 complete

--- exercise2.rb
+++ exercise2.rb
@@ -8,3 +8,12 @@ puts "In other words, the total cost of this $55 meal including a good tip would
 #2. Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
 
 puts "Hello" + 3.to_s
+
+#3 Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+puts "I will use this in a sentence to output 45628 * 7839 which gives a value of #{45628 * 7839}."
+
+#4 What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+# answer is TRUE
+puts "abc"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+puts "abc"
