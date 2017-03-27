386f6b228dd673248c6b25ac81cfaf1b01bc6e27
EDWIN AU <edwinau@rogers.com>
2017-03-27 14:30:38 -0400

    Exercise 2 part 2 complete

--- exercise2.rb
+++ exercise2.rb
@@ -4,3 +4,7 @@ var = 55 * 0.2
 puts "A good tip is 20 percent, making the tip for a $55 meal to be $#{var}."
 puts ""
 puts "In other words, the total cost of this $55 meal including a good tip would be $#{55 + var}"
+
+#2. Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+
+puts "Hello" + 3.to_s
